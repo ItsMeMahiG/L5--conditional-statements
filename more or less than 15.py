a90ad3d46cbdb03c690f8c1cd6b37d1452ff84b9
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-10-08 11:59:50 +0530

    conditional statements

---
+++ more or less than 15.py
@@ -0,0 +1,10 @@
+num=int(input("enter a number"))
+if num<15 :
+    print("the number is less than 15")
+    print("im in the if block")
+
+else :
+    print("the number is more than 15")
+    print("im in the else block")
+
+print("im neither in the if nor else block")
---
+++ profit or loss.py
@@ -0,0 +1,8 @@
+actualCost=float(input("enter the cost of item: "))
+sellingPrice=float(input("enter the selling price of item: "))
+
+if(actualCost<sellingPrice):
+    print("it is a profit!")
+
+else:
+    print("it is a loss")
